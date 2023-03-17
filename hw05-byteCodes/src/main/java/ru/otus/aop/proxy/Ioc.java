4c834f16668e3b8411619d3d159913030be5677d
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2023-03-17 17:00:42 +0300

    Рефлексивный разбор перенесен в конструктор CustomInvocationHandler

--- hw05-byteCodes/src/main/java/ru/otus/aop/proxy/Ioc.java
+++ hw05-byteCodes/src/main/java/ru/otus/aop/proxy/Ioc.java
@@ -3,7 +3,9 @@ package ru.otus.aop.proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 class Ioc {
@@ -18,28 +20,35 @@ class Ioc {
     }
 
     static class CustomInvocationHandler implements InvocationHandler {
-        private final TestLoggingInterface loggingClass;
+        private final TestLoggingInterface loggingInterface;
 
-        CustomInvocationHandler(TestLoggingInterface loggingClass) {
-            this.loggingClass = loggingClass;
+        private final List<Method> methods = new ArrayList<>();
+
+        CustomInvocationHandler(TestLoggingInterface loggingInterface) {
+            this.loggingInterface = loggingInterface;
+            this.methods.addAll(Arrays.stream(loggingInterface.getClass().getMethods()).toList());
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            var classMethod = loggingClass.getClass().getMethod(method.getName(), method.getParameterTypes());
-            if (classMethod.isAnnotationPresent(Log.class)) {
+            var classMethod = methods.stream()
+                    .filter(element ->
+                            element.getName().equals(method.getName()) &&
+                                    Arrays.equals(element.getParameterTypes(), method.getParameterTypes())
+                    ).findFirst();
+            if (classMethod.isPresent() && classMethod.get().isAnnotationPresent(Log.class)) {
                 var params = Arrays.stream(args)
                         .map(Object::toString)
                         .collect(Collectors.joining(", "));
                 System.out.printf("executed method: %s, param: %s%n", method.getName(), params);
             }
-            return method.invoke(loggingClass, args);
+            return method.invoke(loggingInterface, args);
         }
 
         @Override
         public String toString() {
             return "CustomInvocationHandler{" +
-                    "loggingClass=" + loggingClass +
+                    "loggingClass=" + loggingInterface +
                     '}';
         }
     }
